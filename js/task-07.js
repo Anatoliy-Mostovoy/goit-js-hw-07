b69af4598584dbf2d83d481b189f3ec4ae7d9ecf
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-04-19 23:01:58 +0300

    Сделал 7 задание

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#font-size-control');
+const span = document.querySelector('#text');
+
+input.addEventListener('input', onSpanInput);
+
+span.style.fontSize = `${input.value}px`;
+
+function onSpanInput(event){
+    span.style.fontSize = `${event.currentTarget.value}px`; 
+    // span.setAttribute('style',`font-size: ${event.currentTarget.value}px`)
+}
+
